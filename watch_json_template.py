c0422d03ddccebf3aa618b33cf8288536dabc05b
crc8109 <crc8109@pm.me>
2021-04-07 12:13:32 -0400

    Add template for fututre JSON generator

--- watch_json_template.py
+++ watch_json_template.py
@@ -4,17 +4,29 @@ import json
 file_name = input("List brand and AKA (e.g. Aquacy_BronzeBond) for file name: ")
 
 # Sample attributes in future DB
-stats = {"brand" : "",
-         "model": "",
-         # "movement" :"",
-         # "caliber" : "",
-          "type" : ""}
+stats = {
+        "name or aka" : "",
+        "brand" : "",
+        "model": "",
+        "movement" :"",
+        "caliber" : "",
+        "type" : "",
+        "materials": {
+                "case": "",
+                "crystal": "",
+                "strap/band" : ""
+                }
+        }
 
 # To limit user error
 no_mistakes = False
 while no_mistakes == False:
     for attribute in stats:
         stats[attribute] = input(f"insert {attribute}: ")
+
+        if attribute == stats["materials"]:
+            for material in materials["materials"]:
+                materials[material] = input(f"insert {materials}: ")
 
     all_good = eval(input("Is this correct (write True or False)?: "))
     if all_good == True:
